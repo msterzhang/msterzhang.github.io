6a0007e24edacba5ddf034f0f56afeab1726cb36
zhangguanping <114462011@qq.com>
2018-05-21 08:28:13 +0800

    Site updated: 2018-05-21 08:27:56

---
+++ js/biao.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+      var oA = document.getElementsByClassName('biao')
+
+     for(var i= 0;i<oA.length;i++){
+         if (oA[i].innerText=="标签：1")
+     {
+
+         oA[i].innerText="标签：django"
+         }
+                 if (oA[i].innerText=="标签：2")
+     {
+
+         oA[i].innerText="标签：flask学习"
+         }
+              if (oA[i].innerText=="标签：3")
+     {
+
+         oA[i].innerText="标签：爬虫教程"
+         }
+              if (oA[i].innerText=="标签：4")
+     {
+
+         oA[i].innerText="标签：编程项目"
+         }
+           if (oA[i].innerText=="标签：5")
+     {
+
+         oA[i].innerText="标签：生活推荐"
+         }
+     }
+
+  }
